f0d3f1e741fa10c2675811c310e19b3b1c824d63
Robert Shepley <shepleysound@gmail.com>
2022-09-06 11:56:40 -0700

    Implemented connection to secured API endpoints only when signed in to Auth0.

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { PageLayout } from '../components/page-layout';
 
 export const HomePage = () => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userWatchList, setUserWatchList] = useState(null);
 
+  useEffect(() => {
+    const getUserWatchList = async () => {
+      const domain = process.env.REACT_APP_AUTH0_SERVER_URL;
+
+      try {
+        const accessToken = await getAccessTokenSilently();
+
+        // Maybe refactor this later using Axios?
+        const response = await fetch(
+          `${domain}/protected`, 
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const user_metadata = await response;
+
+        setUserWatchList(user_metadata);
+
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+
+    getUserWatchList();
+    
+  }, [getAccessTokenSilently, user?.sub]);
+
+  console.log(userWatchList)
   return (
-    <PageLayout>
-     <div>Hello world!</div>
-    </PageLayout>
+    isAuthenticated && (
+      <PageLayout>
+      <div>Hello world!</div>
+      <div>Hello {user.name}</div>
+      </PageLayout>
+    )
   )
 }
