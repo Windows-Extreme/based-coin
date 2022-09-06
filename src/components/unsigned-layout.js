3f93d6d47f449935ec523eae3e9a08f631c85756
Robert Shepley <shepleysound@gmail.com>
2022-09-05 22:35:54 -0700

    Refine unsigned header buttons.

--- src/components/unsigned-layout.js
+++ src/components/unsigned-layout.js
@@ -4,9 +4,8 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline'
 import AppBar from '@mui/material/AppBar'
 import ToolBar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
 import Typography from '@mui/material/Typography'
-import { Navbar } from './navbar';
+import { UnsignedNavbar } from './unsigned-navbar';
 
 const darkTheme = createTheme({
   palette: {
@@ -22,9 +21,8 @@ export const UnsignedLayout = ({ children }) => {
         <AppBar
           position="fixed"
         >
-          <Navbar />
+          <UnsignedNavbar />
         </AppBar>
-        
         <Box
           component='main'
           sx={{ flexGrow: 1, bgcolor: 'background.detault', p: 3 }}
--- src/components/unsigned-navbar.js
+++ src/components/unsigned-navbar.js
@@ -3,41 +3,26 @@ import React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import { SignupButton } from './navbuttons/signup-button'
 import { LoginButton } from './navbuttons/login-button';
-import { LogoutButton } from './navbuttons/logout-button';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button'
 
-export const Navbar = () => {
-  const { isAuthenticated } = useAuth0();
+export const UnsignedNavbar = () => {
 
   return (
     <Toolbar>
       <Button
         component={Link}
         to='/'
-        >
-        <img src="/logo.svg" alt="logo" height={40}/>
-        </Button>
-      {!isAuthenticated && (
-        <>
-        <SignupButton />
-        <LoginButton />
-        </>
-      )}
-      {isAuthenticated && (
-        <>
-        <LogoutButton />
-        <Button color='inherit'
-          component={Link}
-          to='/home'
-          >Home</Button>
-        </>
-      )}
+      >
+      <img src="/logo.svg" alt="logo" height={40}/>
+      </Button>
       <Button
         color='inherit'
         component={Link}
         to='/about'
         >About Us</Button>
+      <LoginButton />
+      <SignupButton />
     </Toolbar>
   )
 }
