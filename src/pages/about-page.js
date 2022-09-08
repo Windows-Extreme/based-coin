2209e5852ed887e0cfd7c10a685a59a5e82fa7d0
Brandon Pitts <brandomoki.bp@gmail.com>
2022-09-07 19:10:23 -0700

    updated about, profile card, dev profiles

--- src/pages/about-page.js
+++ src/pages/about-page.js
@@ -28,18 +28,8 @@ class AboutUs extends React.Component {
             Coin Fellows
           </Typography>
           <Typography align = "center" variant= "body1" gutterBottom >
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
+
+          Here at Coin Fellows, we are pioneering the future of cryptocurrency and blockchain technology. Our mission is to bring ease of access to the world and enable our users with unbiased and accurate data to allow the user to make their own informed decision.We started in 2022 with the radical idea that anyone, anywhere, should be able to easily and securely track Bitcoin and other Crypto assets. Today, we offer a trusted and easy-to-use platform for accessing the broader cryptoeconomy.
           </Typography>
           </Container>
 
